45d270dcf65b29cad00ffac0bac8520ed780b91f
Miller <millerbarbosa@gmail.com>
2025-11-26 15:24:46 -0300

    avanço na segunda parte do curso

--- public/app/app.js
+++ public/app/app.js
@@ -2,17 +2,19 @@ import { log } from './utils/promise-helper.js';
 import './utils/array-helper.js';
 import { notasService as service } from './nota/servico.js';
 
-const sumItens = code=> notas => notas
-        .$flatMap(nota=>nota.itens)
-        .filter(item => item.codigo ==code)
-        .reduce((total,item) => total + item.valor, 0);
+
+const ehDivisivel = (divisor, numero) => !(numero % divisor);
+
+const fn = ehDivisivel.bind(null, 2, 25)
+
+console.log(fn());
 
 
 document.querySelector('#myButton')
-    .onclick = () => 
-    service
-    .listAll()
-    .then(handleStatus)
-    .then(sumItens('2143'))
-    .then(console.log)
-    .catch(console.log);
+    .onclick = () =>
+        service
+            .listAll()
+            .then(handleStatus)
+            .then(sumItens('2143'))
+            .then(console.log)
+            .catch(console.log);
--- public/app/nota/servico.js
+++ public/app/nota/servico.js
@@ -1,11 +1,30 @@
-import { handle } from 'express/lib/application';
-import {handleStatus} from '../utils/promise-helper.js';
-import
+import { handleStatus } from '../utils/promise-helper.js';
+import { partialize } from '../utils/operators.js';
+import { compose } from '../utils/operators.js';
+
 
 const API = "http://localhost:5000/notas";
 
-export const notasService = () => 
-   
-    listAll(){
-        return fetch(API).then(handleStatus);  
+const getItensFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItensBycode = (code, itens) => itens.filter(item => item.codigo == code);
+
+const sumItensValue = itens => itens.reduce((total, item) => total + item.valor, 0);
+
+
+export const notasService = {
+
+    listAll() {
+        return fetch(API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não é possível obter as notas fiscais');
+            })
+    },
+
+    sumItems(code) {
+        const filterItens = partialize(filterItensBycode, code);
+        return this.listAll().then(sumItens(code));
+
     }
+};
